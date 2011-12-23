ea0ae36d8bc87c6a800d18a9fb906be67ed0caf7
Sarath Lakshman <slakshman@zynga.com>
2011-12-23 10:53:58 +0530

    Added comments in config file

--- src/config.py
+++ src/config.py
@@ -7,15 +7,12 @@ class Config:
     def __init__(self, conf_file = None):
         self.conf_file = conf_file 
         self.config = ConfigParser.ConfigParser()
-        self.db_backup_root = '/db_backup'
         self.s3bucket = 'membase-backup'
-        self.upload_check_interval_mins = 5
         self.log_level = 'INFO'
         self.syslog_tag = 'Membase-Backup-Incr'
 
     def read(self):
         self.config.read(self.conf_file)
-        db_backup_root = self.config.get('Backup', 'db_backup_root')
         s3bucket = self.config.get('S3Upload', 's3bucket')
         backup_interval_mins = self.config.get('Backup', 'backup_interval_mins')
         upload_interval_mins = self.config.get('S3Upload', 'upload_interval_mins')
@@ -28,7 +25,6 @@ class Config:
         download_retries = self.config.get('Restore', 'download_retries')
         upload_retries = self.config.get('S3Upload', 'upload_retries')
 
-        self.db_backup_root = db_backup_root
         self.s3bucket = s3bucket
         self.backup_interval_mins = int(backup_interval_mins)
         self.upload_interval_mins = int(upload_interval_mins)
