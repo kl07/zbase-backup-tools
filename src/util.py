0d207bbc736a3b90ed0a794cf5f1c20a0717f8aa
Sarath Lakshman <slakshman@zynga.com>
2011-12-24 18:07:42 +0530

    SEG-5627 When the backup daemon performs a graceful exit, the subprocesses created using commands.getstatusoutput() was not exiting. They reparented to init Reimplemented getstatusoutput() using subprocess module and disabled shell and parsed the commandline using shlex module

--- src/util.py
+++ src/util.py
@@ -2,6 +2,8 @@
 
 import re
 import os
+import shlex
+
 tokenize = re.compile(r'(\d+)|(\D+)').findall
 def natural_sortkey(string):
     return tuple(int(num) if num else alpha for num, alpha in tokenize(string))
@@ -13,3 +15,18 @@ def setup_sqlite_lib():
     else:
         os.environ['LD_LIBRARY_PATH'] = ld_path
 
+def getcommandoutput(cmd, queue):
+    """Return (status, output) of executing cmd in a shell."""
+    """Add the process object to the queue"""
+    import subprocess
+    args = args = shlex.split(cmd)
+    pipe = subprocess.Popen(args, shell=False, universal_newlines=True,
+            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if queue != None:
+        print "adding item to queue"
+        queue.put(pipe)
+    output = str.join("", pipe.stdout.readlines()) 
+    sts = pipe.wait()
+    if sts is None:
+        sts = 0
+    return sts, output
