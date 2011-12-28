c7d3588543d04dba96c230f34a53930007ee15a8
Sarath Lakshman <slakshman@zynga.com>
2011-12-28 14:29:17 +0530

    Code review: Changed config.py and consts.py

--- src/config.py
+++ src/config.py
@@ -3,42 +3,35 @@
 
 import ConfigParser
 import socket
+import consts
 
 class Config:
     def __init__(self, conf_file = None):
         self.conf_file = conf_file 
         self.config = ConfigParser.ConfigParser()
-        self.s3bucket = 'membase-backup'
-        self.log_level = 'INFO'
-        self.syslog_tag = 'Membase-Backup-Incr'
+        self.s3bucket = consts.S3_BUCKET
+        self.log_level = consts.DEFAULT_LOGLEVEL
+        self.syslog_tag = consts.SYSLOG_TAG
 
     def read(self):
         self.config.read(self.conf_file)
-        s3bucket = self.config.get('S3Upload', 's3bucket')
-        backup_interval_mins = self.config.get('Backup', 'backup_interval_mins')
-        upload_interval_mins = self.config.get('S3Upload', 'upload_interval_mins')
-        log_level = self.config.get('Log', 'log_level')
-        syslog_tag = self.config.get('Log', 'syslog_tag')
-        cloud = self.config.get('General', 'cloud')
-        game_id = self.config.get('General', 'game_id')
-        buffer_list = self.config.get('General', 'buffer_list')
-        try:
-            hostname = self.config.get('Restore', 'hostname')
-        except:
-            hostname = socket.gethostname()
-        download_retries = self.config.get('Restore', 'download_retries')
-        upload_retries = self.config.get('S3Upload', 'upload_retries')
-
-        self.s3bucket = s3bucket
-        self.backup_interval_mins = int(backup_interval_mins)
-        self.upload_interval_mins = int(upload_interval_mins)
+        self.s3bucket = self.config.get('s3', 'bucket')
+        self.backup_interval_mins = int(self.config.get('backup', 'interval'))
+        self.upload_interval_mins = int(self.config.get('backup', 'upload_interval'))
+        log_level = self.config.get('log', 'level')
         if log_level in ['INFO', 'DEBUG']:
             self.log_level = log_level
-        self.syslog_tag = syslog_tag
-        self.cloud = cloud
-        self.game_id = game_id
-        self.hostname = hostname 
-        self.buffer_list = buffer_list 
-        self.download_retries = int(download_retries)
-        self.upload_retries = int(upload_retries)
-        
+
+        self.syslog_tag = self.config.get('log', 'syslog_tag')
+        self.cloud = self.config.get('general', 'cloud')
+        self.game_id = self.config.get('general', 'game_id')
+        self.buffer_list = self.config.get('general', 'buffer_list')
+        try:
+            hostname = self.config.get('restore', 'hostname')
+        except:
+            hostname = socket.gethostname()
+        self.hostname = hostname
+
+        self.download_retries = int(self.config.get('restore', 'download_retries'))
+        self.upload_retries = int(self.config.get('backup', 'upload_retries'))
+
--- src/consts.py
+++ src/consts.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python26
 #Description: Constants definitions
 
-SYSLOG_TAG = 'MembaseBackup'
-MBRESTORE_PIDFILE = '/var/run/mbrestore.pid'
-configfile = '/etc/membase-backup/default.ini'
+SYSLOG_TAG = 'membase-backup'
+MBRESTORE_PID_FILE = '/var/run/mbrestore.pid'
+MBBACKUP_PID_FILE = '/var/run/mbbackup.pid'
+MBMERGE_PID_FILE = '/var/run/mbmerge.pid'
+CONFIG_FILE = '/etc/membase-backup/default.ini'
 LAST_CHECKPOINT_FILE = '/opt/membase/membase-backup/last_closed_checkpoint'
 BACKUP_TAPNAME = 'backup'
 REPLICATION_TAPNAME = 'replication'
@@ -15,6 +17,8 @@ PERIODIC_DIRNAME = 'daily'
 MAX_BACKUP_SEARCH_TRIES = 4
 PATH_MBRESTORE_EXEC = '/opt/membase/lib/python/mbadm-online-restore'
 PATH_MBMERGE_EXEC = '/opt/membase/lib/python/mbbackup-merge-incremental'
+S3_BUCKET = 'membase-backup'
+DEFAULT_LOGLEVEL = 'INFO'
 MERGE_CMD = "/opt/membase/membase-backup/mbmerge-incr"
 BACKUP_ROOT = "/dev/shm"
 MBA_BOOTSTRAP_PATH="membase-backup/mba-hostinfo"
