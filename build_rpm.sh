c0e1ae6464f5cc1e3be6af1745df6e144af5156f
Sarath Lakshman <slakshman@zynga.com>
2012-02-17 13:52:12 +0530

    Minor change to build_rpm.sh

--- build_rpm.sh
+++ build_rpm.sh
@@ -7,12 +7,10 @@ RELEASE=`git log --oneline $VERSION.. | wc -l`
 SOURCE="membase-backup-tools-$VERSION.tar.gz"
 COMMIT=`git describe`
 mkdir -p $RPM_SOURCE_DIR
-mkdir tmp
-cp membase-backup-tools.spec tmp
+cp membase-backup-tools.spec tmp-$$.spec
 sed -i "s/_VERSION/$VERSION/" tmp/membase-backup-tools.spec
 sed -i "s/_SOURCE/$SOURCE/" tmp/membase-backup-tools.spec
 sed -i "s/_RELEASE/$RELEASE/" tmp/membase-backup-tools.spec
 sed -i "s/_COMMIT/$COMMIT/" tmp/membase-backup-tools.spec
 git archive --format=tar --prefix="membase-backup-tools-$VERSION/" HEAD | gzip > $RPM_SOURCE_DIR/$SOURCE
-rpmbuild -ba tmp/membase-backup-tools.spec
-rm -rf tmp
+rpmbuild -ba tmp-$$.spec
