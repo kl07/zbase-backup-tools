0399f95977c29e3d762fc4ea21d1dd6621521579
Sarath Lakshman <slakshman@zynga.com>
2012-03-17 17:17:17 +0530

    Initial commit for merge-incremental
    Highly optimized in-memory dedup based backup merge and split tool

---
+++ src/backup-merge/merge-incremental.cc
@@ -0,0 +1,453 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+
+#include "merge-incremental.hh"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+#include <list>
+#include <fstream>
+#include <sqlite3.h>
+#include <string>
+#include <string.h>
+#include <stdint.h>
+#include <map>
+#include <assert.h>
+#include <libgen.h>
+
+using namespace std;
+
+static const int vbucket_id_idx = 0; 
+static const int op_idx = 1;
+static const int key_idx = 2;
+static const int flag_idx = 3;
+static const int exp_idx = 4;
+static const int cas_idx = 5;
+static const int val_idx = 6;
+static const int cpoint_idx = 7;
+static const int seq_idx = 8;
+
+const char *backup_schema = 
+        "BEGIN; "
+        "CREATE TABLE IF NOT EXISTS cpoint_op" 
+        "(vbucket_id integer, cpoint_id integer, seq integer, op text, "
+        "key varchar(250), flg integer, exp integer, cas integer, val blob,"
+        "primary key(vbucket_id, key));"
+        "CREATE TABLE IF NOT EXISTS cpoint_state"
+        "(vbucket_id integer, cpoint_id integer, prev_cpoint_id integer, state varchar(1), "
+        "source varchar(250), updated_at text);"
+        "COMMIT;" ;
+
+const char *insert_query =
+        "INSERT OR IGNORE into cpoint_op"
+        "(vbucket_id, cpoint_id, seq, op, key, flg, exp, cas, val)"
+        " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)" ;
+
+const char *read_query =
+        "select cpoint_op.vbucket_id,op,key,flg,exp,cas,val,cpoint_id,seq "
+        "from cpoint_op ";
+
+const char *insert_cstate =
+        "INSERT OR IGNORE into cpoint_state"
+        "(vbucket_id, cpoint_id, prev_cpoint_id, state, source, updated_at)"
+        " VALUES (?, ?, ?, ?, ?, ?)";
+
+const char *cpoint_read = "SELECT cpoint_id FROM cpoint_state";
+
+Operation::Operation(uint32_t exp_val, char *key_val, int key_size_val, char *op_val,
+            int op_size_val, char *blob_val, int blob_size_val, uint16_t vbid_val, 
+            uint64_t cpoint_id_val, uint32_t flags_val, uint64_t cas_val, uint64_t seq_val):
+        exp(exp_val), key_size(key_size_val), op_size(op_size_val), blob_size(blob_size_val),
+        vbid(vbid_val), cpoint_id(cpoint_id_val), flags(flags_val), cas(cas_val), seq(seq_val) {
+
+    key = new char[key_size];
+    op = new char[op_size];
+    blob = new char[blob_size];
+    memcpy(key, key_val, key_size);
+    memcpy(op, op_val, op_size);
+    memcpy(blob, blob_val, blob_size);
+
+    size = key_size + op_size + blob_size + sizeof(uint16_t) + sizeof(uint32_t)*3
+        + sizeof(uint64_t)*2 + sizeof(size_t)*4;
+}
+
+Operation::Operation(const Operation& operation):
+        exp(operation.exp), key_size(operation.key_size), op_size(operation.op_size),
+        blob_size(operation.blob_size), vbid(operation.vbid), cpoint_id(operation.cpoint_id),
+        flags(operation.flags), cas(operation.cas), seq(operation.seq) {
+
+    key = new char[operation.key_size];
+    op = new char[operation.op_size];
+    blob = new char[operation.blob_size];
+    memcpy(key, operation.key, key_size);
+    memcpy(op, operation.op, op_size);
+    memcpy(blob, operation.blob, blob_size);
+    size = operation.size;
+}
+
+Operation:: ~Operation() {
+
+    delete [] key;
+    delete [] op;
+    delete [] blob;
+}
+
+bool OutputStore::create_db_name(string &filename) {
+    string tmp = output_file;
+    cout<<"##"<<tmp<<endl;
+    size_t tpos = tmp.find("%");
+    if (tpos > tmp.length()) {
+        filename = tmp;
+        return false;
+    }
+    else {
+        char *buffer = new char[tmp.length() + 5];
+        tmp.erase(tpos, 1);
+        tmp.insert(tpos, "%05d");
+        sprintf(buffer, tmp.c_str(), split_number);
+        filename = buffer; 
+        return true;
+    }
+}
+
+bool OutputStore::initialize_db(string name) {
+    if (sqlite3_open(name.c_str(), &db) != SQLITE_OK) {
+        cout<<"Unable to open db "<<name<<endl;
+        exit(1);
+    }
+    else {
+        cout<<"Creating db - "<<name<<endl;
+    }
+
+    assert(sqlite3_exec(db, backup_schema, 0, 0, 0) == SQLITE_OK);
+    assert(sqlite3_exec(db, "pragma page_size=1024", 0, 0, 0) == SQLITE_OK);
+    assert(sqlite3_exec(db, "pragma count_changes=OFF", 0, 0, 0) == SQLITE_OK);
+    assert(sqlite3_exec(db, "pragma temp_store=MEMORY", 0, 0, 0) == SQLITE_OK);
+    assert(sqlite3_exec(db, "pragma cache_size=1048576", 0, 0, 0) == SQLITE_OK);
+    assert(sqlite3_exec(db, "pragma synchronous=OFF", 0, 0, 0) == SQLITE_OK);
+    assert(sqlite3_exec(db, "pragma journal_mode=MEMORY", 0, 0, 0) == SQLITE_OK);
+    return true;
+}
+
+bool OutputStore::close_db() {
+    sqlite3_stmt *stmt;
+    uint64_t cpoint_id;
+
+    if (db != NULL) {
+        set <int>::iterator it;
+        for (it=checkpoints.begin(); it!=checkpoints.end(); it++) {
+
+            assert(sqlite3_prepare(db, insert_cstate, -1, &stmt, NULL) == SQLITE_OK);
+            sqlite3_bind_int(stmt, 1, 0);
+            sqlite3_bind_int(stmt, 2, (*it));
+            sqlite3_bind_int(stmt, 3, -1);
+            sqlite3_bind_text(stmt, 4, "closed", 6, SQLITE_STATIC);
+            sqlite3_bind_text(stmt, 5, "backup", 6, SQLITE_STATIC);
+            sqlite3_bind_text(stmt, 6, "MERGE", 5, SQLITE_STATIC);
+            assert(sqlite3_step(stmt) == SQLITE_DONE);
+            assert(sqlite3_finalize(stmt)==SQLITE_OK);
+            cpoint_id = *(it);
+        }
+
+        checkpoints.clear();
+        checkpoints.insert(cpoint_id);
+
+        if (enable_split) {
+            assert(sqlite3_exec(db, "COMMIT", 0, 0, 0) == SQLITE_OK);
+        }
+        assert(sqlite3_close(db) == SQLITE_OK);
+        db = NULL;
+    }
+}
+
+OutputStore::OutputStore(string output_file_pattern, set <int> cpoints, int split_size_val): split_number(0), db(NULL), output_file(output_file_pattern), split_size(split_size_val), checkpoints(cpoints) {
+    string filename;
+    enable_split = create_db_name(filename);
+
+    if (split_size == -1) {
+        enable_split = false;
+    }
+
+    initialize_db(filename);
+    sqlite3_stmt *stmt;
+
+    if (enable_split) {
+        assert(sqlite3_exec(db, "BEGIN", 0, 0, 0) == SQLITE_OK);
+    }
+    
+}
+
+bool OutputStore::insert(const Operation *op) {
+    sqlite3_stmt *stmt = NULL;
+    string filename;
+    int page_count = 0;
+    assert(sqlite3_prepare(db, insert_query, -1, &stmt, NULL) == SQLITE_OK);
+    sqlite3_bind_int(stmt, 1, op->vbid);
+    sqlite3_bind_int64(stmt, 2, op->cpoint_id);
+    sqlite3_bind_int64(stmt, 3, op->seq);
+    sqlite3_bind_text(stmt, 4, op->op, op->op_size, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 5, op->key, op->key_size, SQLITE_STATIC);
+    sqlite3_bind_int(stmt, 6, op->flags);
+    sqlite3_bind_int(stmt, 7, op->exp);
+    sqlite3_bind_int64(stmt, 8, op->cas);
+    sqlite3_bind_blob(stmt, 9, op->blob, op->blob_size, SQLITE_STATIC);
+    assert(sqlite3_step(stmt) == SQLITE_DONE);
+    assert(sqlite3_finalize(stmt)==SQLITE_OK);
+
+    assert(sqlite3_prepare(db, "pragma page_count;", -1, &stmt, NULL) == SQLITE_OK);
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+        page_count = sqlite3_column_int(stmt, 0);
+    }
+
+    assert(sqlite3_finalize(stmt)==SQLITE_OK);
+
+    if (enable_split && page_count*1024 > (split_size-1)*1024*1024) {
+        split_number += 1;
+        close_db();
+        create_db_name(filename);
+        initialize_db(filename);
+        assert(sqlite3_exec(db, "BEGIN", 0, 0, 0) == SQLITE_OK);
+    }
+}
+
+OutputStore::~OutputStore() {
+    close_db();
+}
+
+InputStore::InputStore(string file) {
+
+    if (sqlite3_open(file.c_str(), &db) != SQLITE_OK) {
+        cout<<"Unable to open database "<<file<<endl;
+    }
+    assert(sqlite3_exec(db, backup_schema, 0, 0, 0) == SQLITE_OK);
+    assert(sqlite3_exec(db, "pragma page_size=1024", 0, 0, 0) == SQLITE_OK);
+    assert(sqlite3_exec(db, "pragma count_changes=OFF", 0, 0, 0) == SQLITE_OK);
+    assert(sqlite3_exec(db, "pragma temp_store=MEMORY", 0, 0, 0) == SQLITE_OK);
+    assert(sqlite3_exec(db, "pragma cache_size=1048576", 0, 0, 0) == SQLITE_OK);
+    assert(sqlite3_exec(db, "pragma synchronous=OFF", 0, 0, 0) == SQLITE_OK);
+    assert(sqlite3_exec(db, "pragma journal_mode=MEMORY", 0, 0, 0) == SQLITE_OK);
+}
+
+bool InputStore::read() {
+    sqlite3_stmt *stmt = NULL;
+    if (sqlite3_prepare_v2(db, read_query, strlen(read_query), &stmt, NULL) != SQLITE_OK) {
+        (void) sqlite3_finalize(stmt);
+        (void) sqlite3_close(db);
+        cout<<"Statement prepare failed"<<endl;
+        return false;
+    }
+
+    int rc;
+    while ((rc = sqlite3_step(stmt)) != SQLITE_DONE) {
+        if (rc == SQLITE_ROW) {
+            Operation op(
+                    sqlite3_column_int(stmt, exp_idx),
+                    (char*)sqlite3_column_text(stmt, key_idx),
+                    sqlite3_column_bytes(stmt, key_idx),
+                    (char *) sqlite3_column_text(stmt, op_idx),
+                    sqlite3_column_bytes(stmt, op_idx),
+                    (char *) sqlite3_column_text(stmt, val_idx),
+                    sqlite3_column_bytes(stmt, val_idx),
+                    (uint16_t)sqlite3_column_int(stmt, vbucket_id_idx),
+                    (uint64_t)sqlite3_column_int64(stmt, cpoint_idx),
+                    sqlite3_column_int(stmt, flag_idx),
+                    sqlite3_column_int64(stmt, cas_idx),
+                    sqlite3_column_int64(stmt, seq_idx));
+            operations.push_back(op);
+        }
+        else if (rc != SQLITE_BUSY) {
+            cout<<"Sqlite error"<<endl;
+            return false;
+        }
+    }
+
+    assert(sqlite3_finalize(stmt) == SQLITE_OK);
+    assert(sqlite3_close(db) == SQLITE_OK);
+    return true;
+}
+
+InputStore::~InputStore() {
+    operations.clear();
+}
+
+Merge::Merge(list <string> files, string output_file, int split, bool validate): 
+    source_files(files), output_file_pattern(output_file), split_size(split), validation(validate) {
+
+        bool valid = walk_files(source_files, validation);
+        if (validation && !valid) {
+            exit(1);
+        }
+
+        target_store = new OutputStore(output_file_pattern, checkpoints, split_size);
+}
+
+Merge::~Merge() {
+    delete target_store;
+}
+
+void Merge::process() {
+    list <string>::iterator f;
+    f = source_files.begin();
+
+    unsigned int s,e;
+
+    while (f != source_files.end()) {
+        s = time(NULL);
+        cout<<"Processing file - "<<*f<<endl;
+        InputStore is(*f);
+        is.read();
+        list <Operation>::iterator it;
+        for (it = is.operations.begin(); it != is.operations.end(); it++) {
+            Operation *op = &(*it);
+            
+            string key(op->key, 0, op->key_size);
+            if (klist.count(key) == 0) {
+                klist[key] = true;
+                target_store->insert(op);
+            }
+
+        }
+
+        f++;
+
+        e = time(NULL);
+        cout<<"time = "<<e-s<<endl;
+    }
+
+
+}
+
+bool Merge::walk_files(list <string> &files, bool validate) {
+    sqlite3 *tmp_db;
+    sqlite3_stmt *stmt = NULL;
+    uint64_t cpoint_id(0), t(0);
+    list <uint64_t> cpoint_list;
+    list <string>::iterator it;
+    list <uint64_t>::iterator citr;
+
+    for (it=files.begin(); it!=files.end(); it++) {
+        assert(sqlite3_open((*it).c_str(), &tmp_db) == SQLITE_OK);
+        if (sqlite3_prepare_v2(tmp_db, cpoint_read, strlen(cpoint_read), &stmt, NULL) != SQLITE_OK) {
+            cout<<"Statement prepare failed"<<endl;
+            return false;
+        }
+
+        int rc;
+        while ((rc = sqlite3_step(stmt)) != SQLITE_DONE) {
+            if (rc == SQLITE_ROW) {
+                t = sqlite3_column_int64(stmt, 0);
+                cpoint_list.push_back(t);
+                checkpoints.insert(t);
+            }
+            else if (rc != SQLITE_BUSY) {
+                cout<<"Sqlite error"<<endl;
+                return false;
+            }
+
+        }
+        
+        assert(sqlite3_finalize(stmt)==SQLITE_OK);
+        assert(sqlite3_close(tmp_db) == SQLITE_OK);
+
+        if (validate) {
+            cpoint_list.sort();
+            cpoint_list.reverse();
+            t = *(cpoint_list.begin());
+            for (citr=cpoint_list.begin(); citr!=cpoint_list.end(); citr++) {
+                if (!(t == *(citr) || t == *(citr)+1)) {
+                    cout<<"File:"<<(*it)<<" missing checkpoint "<<t-1<<endl;
+                    return false;
+                }
+                else {
+                    if (t == *(citr)+1) {
+                        t--;
+                    }
+                }
+            }
+
+            if (cpoint_id == 0) {
+                cpoint_id = *(cpoint_list.begin());
+            }
+            else {
+                if (!(cpoint_id == *(cpoint_list.begin()) || cpoint_id == *(cpoint_list.begin())+1)) {
+                    cout<<"File:"<<(*it)<<" last_file_cpoint_id:"<<cpoint_id<<" current_file_cpoint_id:"<<*(cpoint_list.begin())<<endl;
+                    return false;
+                }
+                else {
+                    citr = cpoint_list.end();
+                    citr--;
+                    cpoint_id = *(citr);
+                }
+            }
+        }
+        cpoint_list.clear();
+
+    }
+    return true;
+}
+
+int main(int argc, char **argv) {
+    int c;
+    int split_size(-1);
+    int required = 0;
+    bool validation(false);
+    list <string> files;
+    fstream ifs;
+    string inputfile, outputfile;
+    string buffer;
+
+    while ((c = getopt (argc, argv, "i:o:s:v")) != -1) { 
+        switch (c) 
+        {   
+            case 'i':
+                inputfile = optarg;
+                required++;
+                break;
+            case 'o':
+                outputfile = optarg;
+                required++;
+                break;
+            case 's':
+                split_size = atoi(optarg);
+                break;
+            case 'v':
+                validation = true;
+                break;
+            case '?':
+                if (optopt == 'i' || optopt == 'o')
+                    cout<<"Option -"<<(char) optopt<<" requires argument"<<endl;
+                return 1;
+            default:
+                exit(1);
+        }   
+    }
+    
+    if (required != 2) {
+        cout<<"Usage: "<<argv[0]<<" -i files.txt -o file.mbb [-s 512] [-v]"<<endl;
+        exit(1);
+    }
+
+    if (access(inputfile.c_str(), R_OK)) {
+        cout<<"Unable to access backup file list"<<endl;
+        exit(1);
+    }
+
+    cout<<"Input backup list:"<<endl;
+    ifs.open(inputfile.c_str(), fstream::in);
+    while (ifs>>buffer) {   
+        if (access(buffer.c_str(), R_OK)) {
+            cout<<"File "<<buffer<<" cannot be accessed"<<endl;
+            exit(1);
+        }
+
+        files.push_back(buffer);
+        cout<<"File: "<<buffer<<endl;
+    }   
+    cout<<endl;
+
+    Merge mobj(files, outputfile, split_size, validation);
+    mobj.process();
+
+    return 0;
+}
---
+++ src/backup-merge/merge-incremental.hh
@@ -0,0 +1,123 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+
+#ifndef MERGE_HH
+#define MERGE_HH 1
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+#include <sqlite3.h>
+#include <stdint.h>
+#include <set>
+#include <list>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <tr1/unordered_map>
+
+using namespace std;
+
+/**
+ * Operation class is used to store a mutation
+ */
+class Operation {
+
+    uint32_t exp;
+    char *key;
+    char *op;
+    char *blob;
+    uint16_t vbid;
+    uint64_t cpoint_id;
+    uint32_t flags;
+    uint64_t cas;
+    uint64_t seq;
+    size_t blob_size;
+    size_t key_size;
+    size_t op_size;
+    size_t size;
+
+public:
+
+    /**
+     * Constructor that takes the value and create a mutation object
+     */
+    Operation(uint32_t exp_val, char *key_val, int key_size_val, char *op_val,
+            int op_size_val, char *blob_val, int blob_size_val, uint16_t vbid_val, 
+            uint64_t cpoint_id_val, uint32_t flags_val, uint64_t cas_val, uint64_t seq_val);
+    /**
+     * Copy constructor
+     */
+    Operation(const Operation& op);
+
+    /**
+     * Destructor
+     */
+    ~Operation();
+
+    friend class OutputStore;
+    friend class Merge;
+};
+
+
+/**
+ * Storage abstraction for writing mutations
+ */
+class OutputStore {
+    std::string output_file;
+    int split_size;
+    int split_number;
+    bool enable_split ;
+    set <int> checkpoints;
+    sqlite3 *db;
+
+    bool create_db_name(std::string &filename);
+    bool initialize_db(std::string name);
+    bool close_db();
+
+public:
+    OutputStore(std::string output_file_pattern, set <int> cpoints, int split_size_val);
+    bool insert(const Operation *operation);
+    ~OutputStore();
+
+};
+
+/**
+ * Class that models input sqlite files
+ */
+class InputStore {
+    sqlite3 *db;
+    list<Operation> operations;
+
+public:
+
+    InputStore(std::string file);
+    ~InputStore();
+    bool read();
+    friend class Merge;
+};
+
+/**
+ * Merge class which reads the source backup files and merge into deduped
+ * sqlite3 files
+ */
+class Merge {
+    OutputStore *target_store;
+    bool validation;
+    int split_size;
+    string output_file_pattern;
+    std::tr1::unordered_map <std::string, bool> klist;
+    list <std::string> source_files;
+    set <int> checkpoints;
+
+    bool walk_files(list <string> &files, bool validate);
+
+public:
+    Merge(list <string> files, string output_file, int split, bool validate);
+    ~Merge();
+    void process();
+};
+
+#endif
