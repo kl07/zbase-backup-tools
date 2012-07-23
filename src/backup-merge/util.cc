754b34803f22321bdfe1ba194aaeeb580a7f5c58
Sarath Lakshman <slakshman@zynga.com>
2012-07-23 09:37:48 +0530

    Add copy, move, remove generic functions

---
+++ src/backup-merge/util.cc
@@ -0,0 +1,51 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+
+#include "util.hh"
+
+void copyfile(std::string src, std::string dest, bool nocache_input, bool nocache_output) {
+    std::stringstream cmd;
+
+    cmd<<"dd";
+    if (nocache_input)
+    {
+        cmd<<" iflag=direct";
+    }
+
+    if (nocache_output) {
+        cmd<<" oflag=direct";
+    }
+    cmd<<" bs=512k if="<<src<<" of="<<dest<<" 2> /dev/null";
+    assert(system(cmd.str().c_str()) == 0);
+}
+
+void removefile(std::string file) {
+    std::stringstream cmd;
+
+    cmd<<"rm -f "<<file;
+    assert(system(cmd.str().c_str()) == 0);
+}
+
+void movefile(std::string src, std::string dest, bool nocache_input, bool nocache_output) {
+    std::stringstream cmd;
+    copyfile(src, dest, nocache_input, nocache_output);
+    removefile(src);
+}
+
+std::string genfilename(std::string &filename_pattern, int split_no) {
+    size_t tpos;
+    char *buffer;
+    std::string tmp(filename_pattern);
+
+    tpos = tmp.find("%");
+    if (tpos > tmp.length()) {
+        return tmp;
+    } else {
+        buffer = new char[tmp.length() + 5];
+        tmp.erase(tpos, 1);
+        tmp.insert(tpos, "%05d");
+        sprintf(buffer, tmp.c_str(), split_no);
+        tmp.assign(buffer);
+        delete [] buffer;
+        return tmp;
+    }
+}
---
+++ src/backup-merge/util.hh
@@ -0,0 +1,20 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+#ifndef UTIL_HH
+#define UTIL_HH 1
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+
+
+void copyfile(std::string src, std::string dest, bool nocache_input=true, bool nocache_output=true);
+
+void removefile(std::string file);
+
+void movefile(std::string src, std::string dest, bool nocache_input=true, bool nocache_output=true);
+
+std::string genfilename(std::string &filename_pattern, int split_no);
+
+#endif
