7dd174ecef29d5a983ac2c8d5c7d1c63025952db
Sarath Lakshman <slakshman@zynga.com>
2012-12-14 18:47:35 +0530

    Fix build script to use version-release convention for rpm name

--- build_rpm.sh
+++ build_rpm.sh
@@ -21,7 +21,7 @@ cp $specfile tmp-$$.spec
 sed -i "s/_VERSION/$VERSION/" tmp-$$.spec
 sed -i "s/_SOURCE/$SOURCE/" tmp-$$.spec
 sed -i "s/_RELEASE/$RELEASE/" tmp-$$.spec
-sed -i "s/_COMMIT/$COMMIT/" tmp-$$.spec
+sed -i "s/_COMMIT/$VERSION-$RELEASE/" tmp-$$.spec
 git archive --format=tar --prefix="membase-backup-tools-$VERSION/" HEAD | gzip > $RPM_SOURCE_DIR/$SOURCE
 rpmbuild -ba tmp-$$.spec
 rm tmp-$$.spec
