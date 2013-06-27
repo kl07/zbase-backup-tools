36ac229d9567b11bc21e7ea50206d2ef0a8c303c
Sarath Lakshman <slakshman@zynga.com>
2013-06-27 12:44:58 +0530

    Add license

--- src/synctos3.sh
+++ src/synctos3.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 #Description: Sync server backups to s3
 
+#   Copyright 2013 Zynga Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 s3bucket='s3://membase-backup/storage_server_backup'
 server_root='/var/www/html/membase_backup'
 game_id=`grep 'game_id =' /etc/membase-backup/default.ini | awk '{ print $NF }'`
