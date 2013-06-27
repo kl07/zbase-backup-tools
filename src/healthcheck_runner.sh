47619cba456bb0a5b64393787d20c248136b01fe
Sarath Lakshman <slakshman@zynga.com>
2013-06-27 12:43:26 +0530

    Add license

--- src/healthcheck_runner.sh
+++ src/healthcheck_runner.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 #Description: Run health check and send mail if any criticals
 
+#   Copyright 2013 Zynga Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 game_id=`grep 'game_id =' /etc/membase-backup/default.ini | awk '{ print $NF }'`
 rep_dir='/var/www/html/health_reports/'
 count=""
